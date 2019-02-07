8e0e7f56a7f03950a71b80dca57d38df11458510
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-06 16:45:15 -0800

    level 0 module 3

---
+++ sketch_99Bottles/sketch_99Bottles.pde
@@ -0,0 +1,4 @@
+for (int i = 1; i < 99; i--){
+System.out.println();
+
+}
--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -26,6 +26,32 @@ package modulo;
 **/
 
 public class FizzBuzz {
+public static void main(String[] args) {
+	for (int i = 1; i < 21; i++) {
+					 System.out.println(i);
 
+		
+				if (i%3==0) { 
+					System.out.println("fizz");
+				}
+				
+	 if (i%5==0) {
+			System.out.println("buzz");
+	 }
+	 
+			 if (i%3==0 && i%5==0) {
+			System.out.println("fizzbuzz"); 
+			}
+				
+
+		
+		}
+		
+
+		}
+
+	
+	
 }
+
 
